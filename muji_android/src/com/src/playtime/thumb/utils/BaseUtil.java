d2a57d7f9e385131892ac022287797e5306d6598
Xiachar <445322416@qq.com>
2015-06-10 18:04:15 +0800

    15-06-10，wanfei,commit

--- muji_android/src/com/src/playtime/thumb/utils/BaseUtil.java
+++ muji_android/src/com/src/playtime/thumb/utils/BaseUtil.java
@@ -452,4 +452,23 @@ public class BaseUtil {
         }
         return "";
     }
+
+    /**
+     * 字符串转换unicode
+     */
+    public static String string2Unicode(String string) {
+
+        StringBuffer unicode = new StringBuffer();
+
+        for (int i = 0; i < string.length(); i++) {
+
+            // 取出每一个字符
+            char c = string.charAt(i);
+
+            // 转换为unicode
+            unicode.append("\\u" + Integer.toHexString(c));
+        }
+
+        return unicode.toString();
+    }
 }
